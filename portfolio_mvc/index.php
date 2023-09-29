4dcb53bd72a7cf727b100494606cfa9b66972bdd
SkyDogzz <thomas.stephan@live.fr>
2023-09-29 16:20:14 +0200

    create router

--- portfolio_mvc/index.php
+++ portfolio_mvc/index.php
@@ -1,3 +1,35 @@
 <?php
+// Chargez l'autoloader pour gérer automatiquement les classes et les espaces de noms.
+require_once 'vendor/autoload.php';
 
-echo "root index.php";
+// Définissez le chemin de base de l'application (modifiable en fonction de votre configuration).
+define('BASE_PATH', '/');
+
+// Obtenez le chemin de l'URL actuelle (par exemple, "/mon_projet/index.php/projets").
+$current_url = $_SERVER['REQUEST_URI'];
+// Retirez le chemin de base de l'URL pour obtenir le chemin relatif.
+$relative_path = str_replace(BASE_PATH, '', $current_url);
+
+// Divisez le chemin en segments.
+$segments = explode('/', $relative_path);
+
+// Déterminez le contrôleur et la méthode à appeler en fonction des segments de l'URL.
+$controller_name = (isset($segments[1]) && $segments[1] != '') ? ucfirst($segments[1]) . 'Controller' : 'HomeController';
+$method_name = (isset($segments[2]) && $segments[2] != '') ? $segments[2] : 'index';
+
+// Complétez le nom du contrôleur avec l'espace de noms complet.
+$controller_name = 'App\\Controllers\\' . $controller_name;
+
+// Instanciez le contrôleur et appelez la méthode appropriée.
+if (class_exists($controller_name)) {
+    $controller = new $controller_name();
+    if (method_exists($controller, $method_name)) {
+        $controller->$method_name();
+    } else {
+        // Gérez ici la méthode non trouvée (par exemple, affichez une page 404).
+        echo 'Méthode non trouvée';
+    }
+} else {
+    // Gérez ici le contrôleur non trouvé (par exemple, affichez une page 404).
+    echo 'Contrôleur non trouvé';
+}

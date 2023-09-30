7ff16a9275292949b0cb4cb3582834e11314a5c9
SkyDogzz <thomas.stephan@live.fr>
2023-09-30 22:25:23 +0200

    setup browser router

--- portfolio_react/src/App.js
+++ portfolio_react/src/App.js
@@ -1,8 +1,32 @@
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>My Portfolio</h1>
-      <p>Some content</p>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome!</h2>
+      <Outlet />
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
     </div>
   );
 }

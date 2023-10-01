164720206ba08390dc893c5e434018e0a34e8a27
SkyDogzz <thomas.stephan@live.fr>
2023-10-01 15:51:58 +0200

    create authcontroller

---
+++ portfolio_mvc/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Users;
+use Firebase\JWT\JWT;
+use App\Core\BaseController;
+
+class AuthController extends BaseController
+{
+
+    public function login()
+    {
+        $users = new Users();
+        $user = $users->findByColumn('email', 'test@test');
+
+
+        $jwt = JWT::encode((array) $user, $_ENV['JWT_KEY'], 'HS256');
+        echo json_encode($jwt);
+    }
+}

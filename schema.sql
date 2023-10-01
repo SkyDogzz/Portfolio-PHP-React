779557d34637ba3e03c225b025bc0575fb5d7015
SkyDogzz <thomas.stephan@live.fr>
2023-10-01 15:11:58 +0200

    create tables on compose up

--- schema.sql
+++ schema.sql
@@ -1,25 +1,25 @@
 -- Table pour les utilisateurs
-CREATE TABLE utilisateurs (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
 -- Table pour les catégories de projets
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL
 );
 
 -- Table pour les projets
-CREATE TABLE projets (
+CREATE TABLE projects (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
     description TEXT,
-    date_de_creation DATE,
-    utilisateur_id INT,
+    createdAt DATE,
+    user_id INT,
     categorie_id INT,
-    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (categorie_id) REFERENCES categories(id)
 );

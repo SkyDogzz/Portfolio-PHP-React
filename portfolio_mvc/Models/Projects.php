a674700ad5cc09316f212231a906be6d0617ac98
SkyDogzz <thomas.stephan@live.fr>
2023-10-12 06:24:29 +0000

    get projects from databasz=e

--- portfolio_mvc/Models/Projects.php
+++ portfolio_mvc/Models/Projects.php
@@ -4,7 +4,27 @@ namespace App\Models;
 
 use App\Core\Sql;
 
-class Projects extends Sql{
+class Projects extends Sql
+{
 
+    protected int $id;
+    protected string $title;
+    protected string $description;
+    protected $createdAt;
+    protected int $user_id;
+    protected array $categories = [];
 
+    public function findAll(): array
+    {
+        $query = "SELECT projects.*, GROUP_CONCAT(categories.name) AS categories
+        FROM $this->table
+        LEFT JOIN project_categories ON projects.id = project_categories.project_id
+        LEFT JOIN categories ON project_categories.category_id = categories.id
+        GROUP BY projects.id";
+        $db = $this::getInstance();
+        $result = $db->pdo->prepare($query);
+        $result->execute();
+        $projects = $result->fetchAll();
+        return $projects;
+    }
 }
--- portfolio_react/src/components/dashboard/Projects.js
+++ portfolio_react/src/components/dashboard/Projects.js
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Projects() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        axios.post(process.env.REACT_APP_PHP_HOST + ':' + process.env.REACT_APP_PHP_PORT + '/project/')
+            .then((res) => {
+                if (res.data.success) {
+                    console.log(res.data.projects);
+                    setProjects(res.data.projects);
+                }
+                else {
+                    alert(res.data.message);
+                }
+            })
+    }, []);
+
     return (
         <div>
             <h2>Projects</h2>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titre</th>
+                        <th>Description</th>
+                        <th>Categories</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {projects.map((project) => {
+                        return (
+                            <tr key={project.id}>
+                                <td>{project.title}</td>
+                                <td>{project.description}</td>
+                                <td>{project.categories}</td>
+                                <td>
+                                    <button>Modifier</button>
+                                    <button>Supprimer</button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }

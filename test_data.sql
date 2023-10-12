6176c74d2978cf8419a7e2bdd2e7b17ab0471d82
SkyDogzz <thomas.stephan@live.fr>
2023-10-12 08:20:25 +0000

    update test

--- test_data.sql
+++ test_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (name, email, password) VALUES ('John Doe', 'john@doe.com', '123456');
 
-INSERT INTO categories (name) VALUES ('Web');
+INSERT INTO categories (name) VALUES ('Web'), ('PHP'), ('Javascript');
 
-INSERT INTO projects (title, description, createdAt, user_id) VALUES ('Mon premier projet', 'Ceci est la description de mon premier projet', NOW(), 1);
+INSERT INTO projects (title, description, link, github, createdAt, user_id) VALUES ('Mon premier projet', 'Ceci est la description de mon premier projet','http://google.fr', 'https://github.com/skydogzz', NOW(), 1);
 
-INSERT INTO project_categories (project_id, category_id) VALUES (1, 1);
+INSERT INTO project_categories (project_id, category_id) VALUES (1, 1), (1, 2), (1, 3);
